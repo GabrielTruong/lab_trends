a55845fc3a9158b76b6724b46cdea5d67c579dc1
Gabriel TRUONG <lap.gabrieltruong@gmail.com>
2023-01-04 12:41:47 +0100

    chart won't work because of updating dependencies

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
+import plotly.graph_objects as go
 from flask import Flask, render_template
 from pytrends.request import TrendReq
-import plotly.offline as offline
-
-import plotly.express as px
-
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     pytrends = TrendReq()
-    pytrends.build_payload(["machine learning", "deep learning"])
+    pytrends.build_payload(["dog", "cat"])
     trends = pytrends.interest_over_time()
-    print(trends)
-    return render_template("index.html", trends=trends)
-
-
-@app.route("/chart")
-def index():
-    pytrends = TrendReq()
-    pytrends.build_payload(["machine learning", "deep learning"])
-    trends = pytrends.interest_over_time()
-
+    
+    # Create a line chart using Plotly
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=trends.index, y=trends["machine learning"], name="ML"))
-    fig.add_trace(go.Scatter(x=trends.index, y=trends["deep learning"], name="DL"))
+    fig.add_trace(go.Scatter(x=trends.index, y=trends["dog"], name="Dog"))
+    fig.add_trace(go.Scatter(x=trends.index, y=trends["cat"], name="Cat"))
     
-    # Convert the chart to an HTML string
-    chart_html = offline.plot(fig, include_plotlyjs=False, output_type="div")
-    
-    return render_template("index.html", chart_html=chart_html)
+    return render_template("index_chart.html", chart=fig)
+
 
 if __name__ == "__main__":
     app.run()
