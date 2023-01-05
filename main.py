2c897203bc4d86cc6132292c4be4dbaf0e87f208
Gabriel TRUONG <lap.gabrieltruong@gmail.com>
2023-01-05 20:42:36 +0100

    update with chart.js

--- main.py
+++ main.py
@@ -2,20 +2,35 @@ import plotly.graph_objects as go
 from flask import Flask, render_template
 from pytrends.request import TrendReq
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='',
+            static_folder='static',
+            template_folder='templates')
+app.config["DEBUG"] = True
 
-@app.route("/")
-def index():
-    pytrends = TrendReq()
-    pytrends.build_payload(["dog", "cat"])
-    trends = pytrends.interest_over_time()
+@app.route('/chart')
+def chart():
+    trend_dict = get_trend_data()   
+    return render_template('index_chart.html', trend_data=trend_dict)
+
+def get_trend_data():
+    pytrend = TrendReq()
+
+    kw_list = ['dog', 'cat']
+    pytrend.build_payload(kw_list)
+    trend_data = pytrend.interest_over_time()
+    trend_dict = trend_data.to_dict(orient='records')
+
+    return trend_dict
+
+# @app.route("/")
+# def index():
+#     pytrends = TrendReq()
+#     pytrends.build_payload(["dog", "cat"])
+#     trends = pytrends.interest_over_time()
     
-    # Create a line chart using Plotly
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=trends.index, y=trends["dog"], name="Dog"))
-    fig.add_trace(go.Scatter(x=trends.index, y=trends["cat"], name="Cat"))
+
     
-    return render_template("index_chart.html", chart=fig)
+#     return render_template("index_chart.html", chart=fig)
 
 
 if __name__ == "__main__":
